24cf8ec4c432699217149f3b538dde94699183aa
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-29 20:07:55 +0700

    check username

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => res.render('home'));
 
 server.listen(3000, () => console.log('Server started!'));
 
+const arrUsername = [];
+
 io.on('connection', socket => {
     console.log(socket.id);
+    socket.on('DANG_KY_USERNAME', username => {
+        const isExist = arrUsername.indexOf(username) !== -1;
+        if (isExist) return socket.emit('XAC_NHAN_DANG_KY', false);
+        arrUsername.push(username);
+        socket.emit('XAC_NHAN_DANG_KY', true);
+    });
 });
--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 const io = require('socket.io-client');
 const $ = require('jquery');
 
+//init UI
+$('#div-chat').hide();
+
 const socket = io();
+
+$('#btnSignUp').click(() => {
+    const username = $('#txtUsername').val();
+    socket.emit('DANG_KY_USERNAME', username);
+});
